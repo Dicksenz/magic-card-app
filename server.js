8da7de9900ae0047801658a13885657515a8ce7c
Dicksen Veloopillay <dicksenveloopillay@Dicksens-MacBook-Air.local>
2019-09-08 23:44:59 +0400

    Connection table to card clients successful

--- server.js
+++ server.js
@@ -35,8 +35,11 @@ io.on('connection', function(socket){
   // Capture message throw card from cards.html client
   socket.on('throw-card', function(data){
     console.log(data)
+    socket.data = data;
+    io.emit('throw-card-to-table', data);
 
   });
+
 
 });
 
